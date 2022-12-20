0359cd1dade542e38ad1e3ff64facddb7cf0e326
Tomiwa <72953345+oluwatomiwatemidayo@users.noreply.github.com>
2022-12-20 12:09:44 +0100

    Replaced all my files

--- 0x00-ES6_basic/10-loops.js
+++ 0x00-ES6_basic/10-loops.js
@@ -1,8 +1,7 @@
 export default function appendToEachArrayValue(array, appendString) {
-    const newArray = [];
-    for (const value of array) {
-      newArray.push(appendString + value);
-    }
-  
-    return newArray;
+  const array2 = [];
+  for (const value of array) {
+    array2.push(appendString + value);
   }
+  return array2;
+}
